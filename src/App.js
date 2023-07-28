c866781d5e3793e8466168dc0aa8032e15fca445
unknown <becario@EII-D202-00.aulasuo.uniovi.es>
2023-07-28 11:54:42 +0200

    CreateTasksComponent

--- src/App.js
+++ src/App.js
@@ -2,11 +2,9 @@ import './App.css';
 import { useState, useRef } from "react";
 import DetailsOfTasksComponent from './Components/DetailsOfTaskComponent';
 import ListOfTasksComponent from './Components/ListOfTasksComponent';
+import CreateTaskComponent from './Components/CreateTaskComponent';
 
 let App = () => {
-  let [name,setName] = useState("")
-  let [place, setPlace] = useState("");
-  let [priority, setPriority] = useState(0);
 
   let [taskSelected, setTaskSelected] = useState({});
 
@@ -23,30 +21,14 @@ let App = () => {
     }, 
   ])
 
-  let addTask = () => {
-    let newTask = {
-      name: name,
-      place: place,
-      priority: priority
-    }
-    setTasks([...tasks, newTask])
-    setName("")
-    setPlace("")
-    setPriority("")
-  }
+
 
 
   return (
     <div className="container">
       <ListOfTasksComponent tasks={tasks} setTaskSelected={setTaskSelected}/>
 
-      <div className="task-form">
-        <h2>Add task</h2>
-        <input type="text" placeholder='name' value={name} onChange={(e) => { setName(e.currentTarget.value) } }></input>
-        <input type="text" placeholder='place' value={place} onChange={(e) => { setPlace(e.currentTarget.value) } }></input>
-        <input type="number" placeholder='priority' value={priority} onChange={(e) => { setPriority(parseInt(e.currentTarget.value)) } }></input>
-        <button onClick={addTask} > Add task </button>
-      </div>
+      <CreateTaskComponent tasks={tasks} setTasks={setTasks}/>
 
       <DetailsOfTasksComponent task={taskSelected}/>
     </div>
